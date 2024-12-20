f89e58e4204152b0e9540aef9e29c93b7850813a
ansgrb <ansgrb@gmail.com>
2024-12-21 01:33:51 +0200

    adds x-template

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,7 +9,6 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 
 import "../../global.css";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
@@ -18,19 +17,6 @@ SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
   return (
     <>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
